e03e0294c290d61bed0466ccad38666cba3dda6c
Venkatesh <venkatesh@stockgro.com>
2023-09-15 17:25:47 +0530

    video player issue fix when scrolling and upon refreshing the RV adapter.

--- app/src/main/java/com/example/exovideoplayer/PlayerActivity.kt
+++ app/src/main/java/com/example/exovideoplayer/PlayerActivity.kt
@@ -22,9 +22,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
+import androidx.lifecycle.LifecycleOwner
 import androidx.media3.common.AudioAttributes
 import androidx.media3.common.MediaItem
 import androidx.media3.common.MimeTypes
@@ -33,7 +31,6 @@ import androidx.media3.common.Player
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.recyclerview.widget.RecyclerView
 import com.example.exovideoplayer.databinding.ActivityPlayerBinding
-import kotlinx.coroutines.launch
 import java.util.Locale
 import java.util.concurrent.TimeUnit
 
@@ -75,7 +72,6 @@ class PlayerActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(viewBinding.root)
-        scrollView()
         customSeekBar = viewBinding.videoView.findViewById(R.id.custom_seek_bar)
         volumeButton = viewBinding.videoView.findViewById(R.id.volume_button)
         playPauseButton = viewBinding.videoView.findViewById(R.id.play_pause_button)
@@ -95,18 +91,6 @@ class PlayerActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListener {
         initRvAdapter()
     }
 
-    fun scrollView() {
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-                val scrollRuler = viewBinding.scrollableRuler
-                scrollRuler.rulerStartValue = 0
-                scrollRuler.rulerEndValue = 1000
-                scrollRuler.moveToIndex(450)
-                scrollRuler.enableShadow(false)
-            }
-        }
-    }
-
     public override fun onStart() {
         super.onStart()
         if (Build.VERSION.SDK_INT > 23) {
@@ -123,7 +107,7 @@ class PlayerActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListener {
     }
 
     private var recyclerView: RecyclerView? = null
-
+    private val lifecycleOwner: LifecycleOwner = this
     private fun initRvAdapter() {
         recyclerView = viewBinding.videoRecyclerview
         val videoList = mutableListOf(
@@ -131,8 +115,12 @@ class PlayerActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListener {
             getString(R.string.media_url_mp3)
         )
 
-        val adapter = VideoRvAdapter(videoList)
+        val adapter = VideoRvAdapter(videoList, lifecycleOwner = lifecycleOwner)
         recyclerView?.adapter = adapter
+
+        viewBinding.addToFavourites.setOnClickListener {
+            adapter.notifyDataSetChanged()
+        }
     }
 
 
