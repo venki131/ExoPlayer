b9071ae9e2d24fb24fe836a541e7aea6c6d5a283
Venkatesh <venkatesh@stockgro.com>
2023-09-21 12:37:32 +0530

    video scroll listener logic changes

--- app/src/main/java/com/example/exovideoplayer/VideoPlayScrollListener.kt
+++ app/src/main/java/com/example/exovideoplayer/VideoPlayScrollListener.kt
@@ -12,17 +12,24 @@ class VideoPlayScrollListener(
 
         val layoutManager = recyclerView.layoutManager as? LinearLayoutManager ?: return
 
+        // Calculate the height of the RecyclerView
+        val recyclerViewHeight = recyclerView.height
+
         // Get the first fully visible item position
         val firstCompletelyVisibleItemPosition =
             layoutManager.findFirstCompletelyVisibleItemPosition()
 
-        // Attach the player to the first item and partially visible items
+        // Attach the player to the top-most item if it's scrolled at least 50% off the screen
         recyclerView.adapter?.let { rvAdapter ->
             for (i in 0 until rvAdapter.itemCount) {
                 val viewHolder =
                     recyclerView.findViewHolderForAdapterPosition(i) as? VideoViewHolder
-                val attachPlayer =
-                    i == firstCompletelyVisibleItemPosition || isPartiallyVisible(viewHolder)
+                val attachPlayer = shouldAttachPlayer(
+                    i,
+                    firstCompletelyVisibleItemPosition,
+                    recyclerViewHeight,
+                    viewHolder
+                )
                 viewHolder?.apply {
                     if (attachPlayer) {
                         onViewAttachedToWindow(lifecycleOwner)
@@ -34,16 +41,27 @@ class VideoPlayScrollListener(
         }
     }
 
-    private fun isPartiallyVisible(viewHolder: RecyclerView.ViewHolder?): Boolean {
+    private fun shouldAttachPlayer(
+        itemPosition: Int,
+        firstVisiblePosition: Int,
+        recyclerViewHeight: Int,
+        viewHolder: RecyclerView.ViewHolder?
+    ): Boolean {
         viewHolder ?: return false
 
         val view = viewHolder.itemView
         val itemTop = view.top
         val itemBottom = view.bottom
         val itemHeight = view.height
-        val percentageVisible = (itemHeight - (itemBottom - itemTop)) / itemHeight.toFloat()
 
-        return percentageVisible > 0.6f // Adjust the threshold as needed
+        // Calculate the percentage of the item that is above the screen
+        val percentageAboveScreen = (recyclerViewHeight - itemTop).toFloat() / itemHeight
+
+        // Calculate the percentage of the item that is below the screen
+        val percentageBelowScreen = (itemBottom - recyclerViewHeight).toFloat() / itemHeight
+
+        // Check if the item is the top-most item and is scrolled at least 50% off the screen
+        return itemPosition == firstVisiblePosition && (percentageAboveScreen >= 0.5f || percentageBelowScreen >= 0.5f)
     }
 }
 
