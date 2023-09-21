8bc2f58775c5d58fbe03e56ba9b3e9069dca12f3
Venkatesh <venkatesh@stockgro.com>
2023-09-21 15:39:49 +0530

    adding image along with video for selected views and changing the scroll listener logic to attach based on video view instead of itemView.

--- app/src/main/java/com/example/exovideoplayer/VideoPlayScrollListener.kt
+++ app/src/main/java/com/example/exovideoplayer/VideoPlayScrollListener.kt
@@ -1,7 +1,8 @@
 package com.example.exovideoplayer
 
+import android.graphics.Rect
+import android.view.View
 import androidx.lifecycle.LifecycleOwner
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class VideoPlayScrollListener(
@@ -9,59 +10,30 @@ class VideoPlayScrollListener(
 ) : RecyclerView.OnScrollListener() {
     override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
         super.onScrolled(recyclerView, dx, dy)
-
-        val layoutManager = recyclerView.layoutManager as? LinearLayoutManager ?: return
-
-        // Calculate the height of the RecyclerView
-        val recyclerViewHeight = recyclerView.height
-
-        // Get the first fully visible item position
-        val firstCompletelyVisibleItemPosition =
-            layoutManager.findFirstCompletelyVisibleItemPosition()
-
         // Attach the player to the top-most item if it's scrolled at least 50% off the screen
         recyclerView.adapter?.let { rvAdapter ->
             for (i in 0 until rvAdapter.itemCount) {
                 val viewHolder =
                     recyclerView.findViewHolderForAdapterPosition(i) as? VideoViewHolder
-                val attachPlayer = shouldAttachPlayer(
-                    i,
-                    firstCompletelyVisibleItemPosition,
-                    recyclerViewHeight,
-                    viewHolder
-                )
-                viewHolder?.apply {
-                    if (attachPlayer) {
-                        onViewAttachedToWindow(lifecycleOwner)
+                viewHolder?.let {
+                    isVideoPlayerCompletelyVisible(it, viewHolder.itemView)
+                }.apply {
+                    if (this == true) {
+                        viewHolder?.onViewAttachedToWindow(lifecycleOwner)
                     } else {
-                        onViewDetachedFromWindow(lifecycleOwner)
+                        viewHolder?.onViewDetachedFromWindow(lifecycleOwner)
                     }
                 }
             }
         }
     }
-
-    private fun shouldAttachPlayer(
-        itemPosition: Int,
-        firstVisiblePosition: Int,
-        recyclerViewHeight: Int,
-        viewHolder: RecyclerView.ViewHolder?
-    ): Boolean {
-        viewHolder ?: return false
-
-        val view = viewHolder.itemView
-        val itemTop = view.top
-        val itemBottom = view.bottom
-        val itemHeight = view.height
-
-        // Calculate the percentage of the item that is above the screen
-        val percentageAboveScreen = (recyclerViewHeight - itemTop).toFloat() / itemHeight
-
-        // Calculate the percentage of the item that is below the screen
-        val percentageBelowScreen = (itemBottom - recyclerViewHeight).toFloat() / itemHeight
-
-        // Check if the item is the top-most item and is scrolled at least 50% off the screen
-        return itemPosition == firstVisiblePosition && (percentageAboveScreen >= 0.5f || percentageBelowScreen >= 0.5f)
+    // Check if the VideoPlayer view is completely visible within its parent
+    private fun isVideoPlayerCompletelyVisible(videoViewHolder: VideoViewHolder, parent: View): Boolean {
+        val videoPlayerView = videoViewHolder.player // Adjust this to your actual VideoPlayer view
+        val rect = Rect()
+        parent.getLocalVisibleRect(rect)
+        // Check if the visible portion of the VideoPlayer view covers the entire view's height (or width)
+        return videoPlayerView.getLocalVisibleRect(rect) && rect.bottom - rect.top >= videoPlayerView.height
     }
 }
 
