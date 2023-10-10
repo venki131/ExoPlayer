496321d79b823099842ffcd120a9066e7fc9da4d
Venkatesh <venkatesh@stockgro.com>
2023-10-10 17:44:35 +0530

    video URLs changed

--- app/src/main/java/com/example/exovideoplayer/data/repository/SamplePagingSource.kt
+++ app/src/main/java/com/example/exovideoplayer/data/repository/SamplePagingSource.kt
@@ -22,7 +22,15 @@ class SamplePagingSource(
         }
     }
 
-    val videoList = listOf("https://storage.googleapis.com/exoplayer-test-media-0/BigBuckBunny_320x180.mp4", "https://storage.googleapis.com/exoplayer-test-media-0/Jazz_In_Paris.mp3")
+    val videoList = listOf(
+        /*"https://storage.googleapis.com/exoplayer-test-media-0/BigBuckBunny_320x180.mp4",
+        "https://storage.googleapis.com/exoplayer-test-media-0/Jazz_In_Paris.mp3",*/
+        "https://storage.googleapis.com/stockgro-feed-assets-qa/post_media/428de1c3-03d2-46a5-a5b8-2bab2eca9850.mp4",
+        "https://storage.googleapis.com/stockgro-feed-assets-qa/post_media/5b007f34-5ecb-4d0f-8972-fa643ad528b5.mp4",
+        "https://storage.googleapis.com/stockgro-feed-assets-qa/post_media/07ef8030-d627-47bc-a09d-b0a34514c3f8.mp4",
+        "https://storage.googleapis.com/stockgro-feed-assets-qa/post_media/b5ad16e9-01d6-4a29-9b6f-cb68069f4d85.mp4",
+        "https://storage.googleapis.com/stockgro-feed-assets-qa/post_media/8f55abf0-ffd5-42d3-8253-64a28470ac9d.mp4"
+    )
 
     override suspend fun load(params: LoadParams<String>): LoadResult<String, String> {
         return try {
